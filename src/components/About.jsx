3e076bb59689043a24c93d3368858501abb5a06e
lilmubby <ymubarak247@gmail.com>
2023-01-06 17:02:04 +0100

    reduce font colors in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,17 +15,17 @@ function About() {
         I'm a self-taught
         <span className="text-red-500 font-bold" > frontend developer</span> with over 
         <span className="text-black font-bold" > a year of experience</span> building 
-        <span className="text-blue-700 font-extrabold" > responsive web apps</span> using 
-        <span className="text-red-500 font-bold" > frontend technology. </span> 
+        <span className="text-blue-00 font-bold" > responsive web apps</span> using 
+        <span className="text-blue-700 font-bold" > frontend technology. </span> 
         I have received sunstantial and comprehensive knowledge  
         in these stacks and also,
         constantly learning to hone my skill-sets. I enjoy problem solving and bringing ideas 
         to live. I'm fascinated by 
-        <span className="text-red-400 font-bold" > user experience</span>, 
+        <span className="text-red-500 font-bold" > user experience</span>, 
         <span className="text-black font-bold" > creative interface</span>, 
-        <span className="text-blue-500 font-bold" > accessibility</span>, 
-        <span className="text-red-500 font-bold" > games </span> and 
-        <span className="text-blue-500 font-bold" > blockchain technology</span>
+        <span className="text-blue-00 font-bold" > accessibility</span>, 
+        <span className="text-red-00 font-bold" > games </span> and 
+        <span className="text-blue-700 font-bold" > blockchain technology</span>
       </p>
     </section>
   )
